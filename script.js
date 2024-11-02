852dc8b63c0f72cbe0e8667bbc9f374800449e24
kanimozhis-4 <kanimozhi808088@gmail.com>
2024-11-02 16:44:46 +0530

    updated the color

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to update prices based on toggle
 const updatePrices = () => {
     const isChecked = document.getElementById('price-toggle').checked;
     const cards = document.getElementsByClassName('amount');
@@ -7,12 +6,10 @@ const updatePrices = () => {
         const monthlyPrice = card.getAttribute('data-monthly');
         const annualPrice = card.getAttribute('data-annual');
 
-        // Set the price based on the toggle state
         card.textContent = isChecked ? monthlyPrice : annualPrice;
     });
 };
 
-// Add event listener to the checkbox
 document.getElementById('price-toggle').addEventListener('change', updatePrices);
 
 updatePrices();
